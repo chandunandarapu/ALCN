ee41ebc5d1ce2272420e8e21f7f548ae8a5548bf
Chandu Nandarapu <183835210+chandunandarapu@users.noreply.github.com>
2026-05-29 20:56:05 +0530

    Completed Phase 2 REST API Development

--- backend/apps/portfolio/models.py
+++ backend/apps/portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class Portfolio(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    github_url = models.URLField()
+    live_url = models.URLField(blank=True, null=True)
+    image = models.ImageField(upload_to='portfolio/')
+    created_at = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.title
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/', include('apps.users.urls')),
+    path('api/', include('apps.clients.urls')),
+    path('api/', include('apps.projects.urls')),
+    path('api/', include('apps.tasks.urls')),
+    path('api/', include('apps.portfolio.urls')),
 ]
